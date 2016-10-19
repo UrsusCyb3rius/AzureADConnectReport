37dd8d342d27b0cc16eb290de5e5fa32504dad76
Michael Van Horenbeeck <michael@vanhorenbeeck.be>
2016-10-19 11:13:06 +0200

    File creation
    Initial file creation

---
+++ New-AzureADConnectReport.ps1
@@ -0,0 +1,14 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2016 v5.2.128
+	 Created on:   	10/19/2016 10:46 AM
+	 Created by:   	Michael Van Horenbeeck
+	 Organization: 	VH Consulting & Training
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+#GIT TEST
